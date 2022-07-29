75976472f2410fd646be86daf63d7c9ba6cf57a3
DilshanHarendra <harendracripto@gmail.com>
2022-07-29 12:10:25 +0530

    Initial Commit

---
+++ netflix_clone_swiftui/netflix_clone_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_clone_swiftuiApp.swift
+//  netflix_clone_swiftui
+//
+//  Created by Dilshan Harendra Perera on 2022-07-29.
+//
+
+import SwiftUI
+
+@main
+struct netflix_clone_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
